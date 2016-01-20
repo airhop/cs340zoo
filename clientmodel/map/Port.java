18674869a4d0e52af422770354e44402c5148b4c
airhop <airhop12@gmail.com>
2016-01-19 23:07:29 -0700

    Update Port.java

--- clientmodel/map/Port.java
+++ clientmodel/map/Port.java
@@ -1,5 +1,18 @@
 package clientmodel.map;
 
 public class Port {
+  
+  HexLocation location;
+  String resource;
+  String direction;
+  int ratio;
+  
+  Port(int x, int y, String resrc, String dir, int rat)
+  {
+    location = new HexLocation(x,y);
+    resource = resrc;
+    direction = dir;
+    ratio = rat;
+  }
 
 }
