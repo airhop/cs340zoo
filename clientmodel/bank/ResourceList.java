e6ec5d1fed43e46c615eb2c36642b7650cad3338
airhop <airhop12@gmail.com>
2016-01-19 17:52:39 -0700

    Update ResourceList.java

--- clientmodel/bank/ResourceList.java
+++ clientmodel/bank/ResourceList.java
@@ -2,10 +2,19 @@ package clientmodel.bank;
 
 public class ResourceList {
   
-  int numOfBrick;
+    int numOfBrick;
   int numOfOre;
   int numOfSheep;
   int numOfWheat;
   int numOfWood;
+  
+  ResourceList()
+  {
+  numOfBrick = 19;
+  numOfOre = 19;
+  numOfSheep = 19;
+  numOfWheat = 19;
+  numOfWood = 19;
+  }
 
 }
