b887873b90d14a53c4c796b6198a4dfda1ea428c
airhop <airhop12@gmail.com>
2016-01-19 23:18:46 -0700

    Update VertexObject.java

--- clientmodel/map/VertexObject.java
+++ clientmodel/map/VertexObject.java
@@ -1,1 +1,12 @@
+package clientmodel.map;
 
+public class VertexObject{
+  int owner;
+  EdgeLocation location;
+  
+  VertexObject(int x, int y, int id)
+  {
+    location = new EdgeLocation(x,y);
+    owner = id;
+  }
+}
