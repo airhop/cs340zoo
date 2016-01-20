a41868e92243eac944df6a65a9d4c6cba800e70d
airhop <airhop12@gmail.com>
2016-01-20 12:56:45 -0700

    Update Bank.java

--- clientmodel/bank/Bank.java
+++ clientmodel/bank/Bank.java
@@ -2,6 +2,46 @@
 package clientmodel.bank;
 
 public class Bank {
-  ResourceList resources = new ResourceList();
-
+  ResourceList resources = new ResourceList("map");
+  
+  addBrick()
+  {
+    
+  }
+  giveBrick()
+  {
+    
+  }
+  addOre()
+  {
+    
+  }
+  giveOre()
+  {
+    
+  }
+  addSheep()
+  {
+    
+  }
+  giveSheep()
+  {
+    
+  }
+  addWheat()
+  {
+    
+  }
+  giveWheat()
+  {
+    
+  }
+  addWood()
+  {
+    
+  }
+  giveWood()
+  {
+    
+  }
 }
