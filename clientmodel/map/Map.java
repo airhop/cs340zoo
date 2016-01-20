91106dd3def54d8dfeef1569bf579d2339b51693
airhop <airhop12@gmail.com>
2016-01-20 12:34:44 -0700

    Update Map.java

--- clientmodel/map/Map.java
+++ clientmodel/map/Map.java
@@ -6,5 +6,33 @@ public class Map {
   ArrayList<Road> roads = new ArrayList <Road> ();
   ArrayList<VertexObject> settlements = new ArrayList<VertexObject> ();
   ArrayList<VertexObject> cities = new ArrayList<VertexObject> ();
+  int radius = -1;
+  HexLocation robber = null;
   
+ addHex(int x, int y, String resource, int number)
+ {
+   
+ }
+ addPort(int x, int y, String resource, String direction, int ratio)
+ {
+   
+ }
+ addRoad(int x, int y, String direction, int owner)
+ {
+   
+ }
+ addSettlement(int x, int y, String direction, int owner)
+ {
+   
+ }
+ addCity(int x, int y, String direction, int owner)
+ {
+   
+ }
+ relocateRobber(int x, int y)
+ {
+   
+ }
+ 
+ //getters and setters
 }
