f9aa8d93e8fbb4b7bc37b5a9809244b02755917b
Saladin926 <davidtostler@gmail.com>
2016-01-20 01:39:19 -0700

    update TurnTracker.java
    my good man

--- clientmodel/misc/TurnTracker.java
+++ clientmodel/misc/TurnTracker.java
@@ -1,5 +1,15 @@
 package clientmodel.misc;
 
 public class TurnTracker {
-
+  int currPlayer;
+  String status;
+  int longestRoad;
+  int largestArmy;
+  
+  void startTurn(){}
+  void endTurn(){}
+  void updateStatus(){}
+  void calcLargestArmy(){}
+  void calcLongestroad(){}
+  
 }
