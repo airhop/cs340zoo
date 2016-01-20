15f87404f93ab224ae959936f5c2286e5ab0990e
airhop <airhop12@gmail.com>
2016-01-19 22:58:33 -0700

    Update Hex.java

--- clientmodel/map/Hex.java
+++ clientmodel/map/Hex.java
@@ -1,5 +1,16 @@
 package clientmodel.map;
 
 public class Hex {
-
+  
+  HexLocation location;;
+  String resource;
+  String number;
+  
+  Hex(int x, int y, String resrc, String num)
+  {
+    location = new HexLocation(x,y);
+    resource = resrc;
+    number = num;
+  }
+  //Getters and Setters
 }
