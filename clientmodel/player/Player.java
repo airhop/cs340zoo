77629470a84fec4b15e608e2c0c0953702c3cf0a
Saladin926 <davidtostler@gmail.com>
2016-01-20 01:22:29 -0700

    Update Player.java
    if you would

--- clientmodel/player/Player.java
+++ clientmodel/player/Player.java
@@ -1,5 +1,31 @@
 package clientmodel.player;
 
 public class Player {
-
+  
+  int cities;
+  String color;
+  boolean discarded;
+  int monuments;
+  String name;
+  DevCardList newDevCards;
+  DevCardList oldDevCards;
+  boolean playedDevCard;
+  int playerID;
+  ResourceList resources;
+  int roads;
+  int settlements;
+  int soldiers;
+  int victoryPoints;
+  
+  
+  void addVictoryPoint(){}
+  void drawDevCard(){}
+  void placeCity(){}
+  void playCard(){}
+  void trade(){}
+  void purchase(){}
+  void endTurn(){}
+  void playMonument(){}
+  
+  //getters and setters
 }
