da1ea25b9c0953283fd9f4e0ab195dcbceb77daa
airhop <airhop12@gmail.com>
2016-01-20 12:08:33 -0700

    Update Map.java

--- clientmodel/map/Map.java
+++ clientmodel/map/Map.java
@@ -1,5 +1,10 @@
 package clientmodel.map;
 
 public class Map {
-
+  ArrayList<Hex> hexes = new ArrayList <Hex> ();
+  ArrayList<Port> ports = new ArrayList <Port> ();
+  ArrayList<Road> roads = new ArrayList <Road> ();
+  ArrayList<VertexObject> settlements = new ArrayList<VertexObject> ();
+  ArrayList<VertexObject> cities = new ArrayList<VertexObject> ();
+  
 }
