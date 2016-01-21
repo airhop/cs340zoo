54398017234fe3bbdfed9cad15e06b05ad434573
Joshua <josh.w.nielsen@gmail.com>
2016-01-21 14:25:19 -0700

    Poller Methods

--- client/poller/Poller.java
+++ client/poller/Poller.java
@@ -1,5 +1,20 @@
 package clientmodel.poller;
 
 public class Poller {
+    IProxy myProxy;
+    ClientModel myModel;
 
+    public Poller(IProxy givenProxy, ClientModel givenModel){
+        myProxy = givenProxy;
+        myModel = givenModel;
+    }
+
+    /**
+     * Every few seconds the poller grabs the model from the server
+     * and updates the ClientsModel
+     * @return
+     */
+    public ClientModel getClientModel(){
+
+    }
 }
