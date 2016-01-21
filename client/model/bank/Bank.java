8b7bcd37561b673b06ebe1bb74636c51bd0e0a1d
airhop <airhop12@gmail.com>
2016-01-21 16:21:54 -0700

    Update Bank.java

--- client/model/bank/Bank.java
+++ client/model/bank/Bank.java
@@ -4,97 +4,141 @@ package clientmodel.bank;
 public class Bank {
   ResourceList resources = new ResourceList("map");
     /**
-    * checks to make sure the bank can give the proper resource amount
+    * checks to make sure the bank can add the proper resource amount
     */
   canAddBrick()
   {
     
   }
-  
+    /**
+    * adds proper resource to resourcelist
+    */
   addBrick()
   {
     
   }
+    /**
+    * checks to make sure the bank can give the proper resource amount
+    */  
   canGiveBrick()
   {
     
   }
+     /**
+    * gives proper resource from resourcelist
+    */  
   giveBrick()
   {
     
   }
     /**
-    * checks to make sure the bank can give the proper resource amount
+    * checks to make sure the bank can add the proper resource amount
     */
   canAddOre()
   {
     
   }
+     /**
+    * adds proper resource to resourcelist
+    */  
   addOre()
   {
     
   }
+    /**
+    * checks to make sure the bank can give the proper resource amount
+    */  
   canGiveOre()
   {
     
   }
+    /**
+    * gives proper resource from resourcelist
+    */  
   giveOre()
   {
     
   }
     /**
-    * checks to make sure the bank can give the proper resource amount
+    * checks to make sure the bank can add the proper resource amount
     */  
   canAddSheep()
   {
     
   }
+    /**
+    * adds proper resource to resourcelist
+    */
   addSheep()
   {
     
   }
+    /**
+    * checks to make sure the bank can give the proper resource amount
+    */  
   canGiveSheep()
   {
     
   }
+    /**
+    * gives proper resource from resourcelist
+    */  
   giveSheep()
   {
     
   }
     /**
-    * checks to make sure the bank can give the proper resource amount
+    * checks to make sure the bank can add the proper resource amount
     */  
   canAddWheat()
   {
     
   }
+    /**
+    * adds proper resource to resourcelist
+    */
   addWheat()
-  {
-    
-  }
-  canGiveWheat()
-  {
-    
-  }
-  giveWheat()
   {
     
   }
     /**
     * checks to make sure the bank can give the proper resource amount
     */  
+  canGiveWheat()
+  {
+    
+  }
+    /**
+    * gives proper resource from resourcelist
+    */  
+  giveWheat()
+  {
+    
+  }
+    /**
+    * checks to make sure the bank can add the proper resource amount
+    */  
   canAddWood()
   {
     
   }
+    /**
+    * adds proper resource to resourcelist
+    */  
   addWood()
   {
     
   }
+    /**
+    * checks to make sure the bank can give the proper resource amount
+    */  
   canGiveWood()
   {
     
   }
+    /**
+    * gives proper resource from resourcelist
+    */  
   giveWood()
   {
     
