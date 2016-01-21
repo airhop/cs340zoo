1026a3dd8bc465c6ca660133da995e62c48f6b25
airhop <airhop12@gmail.com>
2016-01-21 12:17:17 -0600

    Update ClientModel.java

--- client/model/ClientModel.java
+++ client/model/ClientModel.java
@@ -4,10 +4,19 @@ public class ClientModel {
   int version = 0;
   int winner = -1;
   
+    /**
+    * updates version of the game model
+    *
+    */
   public void updateVersion()
   {
     version++;
   }
+    /**
+    * decides game winner and sets the winner to the id of the winning player
+    * 
+    * @param int winnerid - id of the winning player
+    */
   public void decideWinner(int winnerid)
   {
     if(winnerid<0 || winnerid >3)
