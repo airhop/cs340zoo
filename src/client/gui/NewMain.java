9063b7e6ef9d0dc22f51969e12e52bc0ef1c4aa4
Joshua <josh.w.nielsen@gmail.com>
2016-01-22 01:36:29 -0700

    Runnable Code!! Woot

---
+++ src/client/gui/NewMain.java
@@ -0,0 +1,10 @@
+package client.gui;
+
+/**
+ * Created by Joshua on 1/22/2016.
+ */
+public class NewMain {
+    public static void main(String[] args){
+        System.out.println("This cow");
+    }
+}
