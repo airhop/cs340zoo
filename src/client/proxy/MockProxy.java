2dc20fcdd44e4ab488b52c7f9ff13a57b149e0f4
Joshua <josh.w.nielsen@gmail.com>
2016-01-22 14:31:51 -0700

    Runnable Code!! Woot

--- src/client/proxy/MockProxy.java
+++ src/client/proxy/MockProxy.java
@@ -1,6 +1,11 @@
 package client.proxy;
 
 import client.model.ClientModel;
+import client.model.bank.ResourceList;
+import client.model.history.MessageList;
+import client.model.map.EdgeLocation;
+import client.model.map.HexLocation;
+import shared.jsonobject.Login;
 import shared.jsonobject.PassObject;
 
 public class MockProxy implements IProxy{
@@ -16,88 +21,149 @@ public class MockProxy implements IProxy{
 
     }
 
+
     @Override
-    public ClientModel getGameModel(PassObject pass) {
-        return null;
+    public boolean userLogin(Login l) {
+        return false;
     }
 
     @Override
-    public ClientModel placeRoad(PassObject pass) {
-        return null;
+    public boolean userRegister(Login l) {
+        return false;
     }
 
     @Override
-    public ClientModel placeSettlement(PassObject pass) {
-        return null;
+    public String[] gamesList() {
+        return new String[0];
     }
 
     @Override
-    public ClientModel placeCity(PassObject pass) {
-        return null;
+    public boolean gamesCreate(String s) {
+        return false;
     }
 
     @Override
-    public ClientModel buyDevCard(PassObject pass) {
-        return null;
+    public boolean gamesJoin(String s, int playerId) {
+        return false;
     }
 
     @Override
-    public ClientModel playDevCard(PassObject pass) {
-        return null;
+    public boolean gameAddAI() {
+        return false;
     }
 
     @Override
-    public ClientModel playMonopoly(PassObject pass) {
-        return null;
+    public String[] gameListAI() {
+        return new String[0];
     }
 
     @Override
-    public ClientModel playRoadBuilding(PassObject pass) {
-        return null;
+    public void sendChat(MessageList msg) {
+
     }
 
     @Override
-    public ClientModel placeMonument(PassObject pass) {
-        return null;
+    public void finishTurn(int playerId) {
+
     }
 
     @Override
-    public ClientModel playYearOfPlenty(PassObject pass) {
-        return null;
+    public void buildCity(int playerId, EdgeLocation el) {
+
     }
 
     @Override
-    public ClientModel placeSoldier(PassObject pass) {
-        return null;
+    public void buildSettlement(int playerId, EdgeLocation el) {
+
     }
 
     @Override
-    public ClientModel rob(PassObject pass) {
-        return null;
+    public void discardCards(int playerId, ResourceList rl) {
+
     }
 
     @Override
-    public ClientModel moveRobber(PassObject pass) {
-        return null;
+    public void rollNumber(int numRoled) {
+
     }
 
     @Override
-    public ClientModel tradePlayer(PassObject pass) {
-        return null;
+    public void getGameModel() {
+
     }
 
     @Override
-    public ClientModel tradeBank(PassObject pass) {
-        return null;
+    public void placeRoad(int playerId, EdgeLocation el) {
+
     }
 
     @Override
-    public ClientModel acceptTrade(PassObject pass) {
-        return null;
+    public void placeSettlement(int playerId, EdgeLocation el) {
+
     }
 
     @Override
-    public ClientModel win(PassObject pass) {
-        return null;
+    public void placeCity(int playerId, EdgeLocation el) {
+
+    }
+
+    @Override
+    public void buyDevCard(int playerId) {
+
+    }
+
+    @Override
+    public void playMonopoly(int playerId, String card) {
+
+    }
+
+    @Override
+    public void playRoadBuilding(int playerId, EdgeLocation e1, EdgeLocation e2) {
+
+    }
+
+    @Override
+    public void placeMonument(int playerId) {
+
+    }
+
+    @Override
+    public void playYearOfPlenty(int playerId) {
+
+    }
+
+    @Override
+    public void playSoldier(int playerId) {
+
+    }
+
+    @Override
+    public void robPlayer(int playerIdOne, int playerIdTwo) {
+
+    }
+
+    @Override
+    public void moveRobber(int playerId, HexLocation hl) {
+
+    }
+
+    @Override
+    public void tradePlayer(int playerIdOne, int playerIdTwo, ResourceList rl) {
+
+    }
+
+    @Override
+    public void tradeBank(int playerId, ResourceList rl) {
+
+    }
+
+    @Override
+    public void acceptTrade(int playerIdOne, int playerIdTwo, ResourceList rl) {
+
+    }
+
+    @Override
+    public void win(int playerId) {
+
     }
 }
