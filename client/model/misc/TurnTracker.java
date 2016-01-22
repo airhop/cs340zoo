7aa595870f63ce1da2524091eab8eb8ec3a16afa
KasiKarra <hate2shop@sbcglobal.net>
2016-01-21 20:23:00 -0700

    Update TurnTracker.java
    calcLA, calcLR - might return a bool to let the player if they got the card, but not entirely sure if it is necessary at the moment

--- client/model/misc/TurnTracker.java
+++ client/model/misc/TurnTracker.java
@@ -1,15 +1,31 @@
-package clientmodel.misc;
+package client.model.misc;
 
 public class TurnTracker {
   int currPlayer;
   String status;
   int longestRoad;
+  int currLR = 4; //see note about largest army
   int largestArmy;
+  int currLA = 2;  
+  //must be at least 3 to claim largest army, therefore if player army >  currLA they earn largest army and currLA = player army size
   
-  void startTurn(){}
-  void endTurn(){}
-  void updateStatus(){}
-  void calcLargestArmy(){}
-  void calcLongestroad(){}
+  public TurnTracker(){}
+//  void startTurn(){}
+//  void endTurn(){}
+
+/*@param int pid
+if not turn don't do anything, else move turn to next player
+*/
+  void updateStatus(int pid){}
+  
+/*@param pid - player id of player wishing to try for largest army
+    might return bool in future, not a necessity at the moment though
+*/
+  void calcLargestArmy(int pid){}
+  
+  /*@param pid = player id of player wishing to try for longest road
+      again might return bool in future, not a necessity at the moment
+  */
+  void calcLongestRoad(int pid){}
   
 }
