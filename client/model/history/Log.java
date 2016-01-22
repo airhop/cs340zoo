e27b2cac1344d1d43a4bd01a9730b544b616c18f
KasiKarra <hate2shop@sbcglobal.net>
2016-01-21 20:43:20 -0700

    Update Log.java

--- client/model/history/Log.java
+++ client/model/history/Log.java
@@ -4,6 +4,12 @@ public class Log {
 
 	MessageList messageList;
 
+	/*for printing or returning the list, however the history will need it.
+	*/
 	void printMessageList(){}
+	
+	/*to add a message to the messagelist
+	*/
+	void addMessage(String source, String message){}
 
 }
