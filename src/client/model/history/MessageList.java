81fcf9ef1533e69f182884c08c4888deeb838878
airhop <airhop12@gmail.com>
2016-01-28 14:51:29 -0700

    Update MessageList.java

--- src/client/model/history/MessageList.java
+++ src/client/model/history/MessageList.java
@@ -4,26 +4,16 @@ import java.util.ArrayList;
 
 public class MessageList {
 
-  ArrayList<MessageLine> chatMessages;
-  ArrayList<MessageLine> logMessages;
+  ArrayList<MessageLine> messages;
   
   /**
-  * Adds a message to the chat list
+  * Adds a message to the message list
   * 
   * @param line - a line of text
   */
-  public void addChatMessage(MessageLine line)
+  public void addMessage(MessageLine line)
   {
-    chatMessages.add(line);
-  }
-   /**
-  * Adds a message to the log list
-  * 
-  * @param line - a line of text
-  */
-  public void addLogMessage(MessageLine line)
-  {
-    logMessages.add(line);
+    messages.add(line);
   }
    /**
   * Gets the line from the chat list that matches the paramater line
@@ -31,30 +21,13 @@ public class MessageList {
   * 
   * @param line - a line of text
   */
-  public MessageLine getChatLine(MessageLine line)
+  public MessageLine getMessageLine(MessageLine line)
   {
   	for(int i = 0; i < chatMessages.getLength(); i++)
   	{
   	  if(chatMessages.get(i).getMessage.equals(line.getMessage) && chatMessages.get(i).getMessage.equals(line.getSource))
   	  {
   	    return chatMessages.get(i);
-  	  }
-  	}
-  	return null;
-  }
-     /**
-  * Gets the line from the log list that matches the paramater line
-  * If there is an exact match it will return it, otherwise it will return null
-  * 
-  * @param line - a line of text
-  */
-  public MessageLine getLogLine()
-  {
-  	for(int i = 0; i < chatMessages.getLength(); i++)
-  	{
-  	  if(logMessages.get(i).getMessage.equals(line.getMessage) && chatMessages.get(i).getMessage.equals(line.getSource))
-  	  {
-  	    return logMessages.get(i);
   	  }
   	}
   	return null;
