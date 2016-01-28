566977f029183cca1b145ff4c2032a852c388329
airhop <airhop12@gmail.com>
2016-01-28 14:55:26 -0700

    Update MessageLine.java

--- src/client/model/history/MessageLine.java
+++ src/client/model/history/MessageLine.java
@@ -6,6 +6,12 @@ public class MessageLine {
 String message;
 String source;
 
+MessageLine(String mess, String src)
+{
+  message = mess;
+  source = src;
+}
+
 //getters and setters right here
 
 }
