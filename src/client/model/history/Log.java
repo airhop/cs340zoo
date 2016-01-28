4c5ec7c2fc0d0683bed17cd71a6aadc056530629
airhop <airhop12@gmail.com>
2016-01-28 15:00:35 -0700

    Update Log.java

--- src/client/model/history/Log.java
+++ src/client/model/history/Log.java
@@ -3,15 +3,27 @@ package client.model.history;
 public class Log {
 
 	MessageList LogList;
-
 	/**
 	 * for printing or returning the list, however the history will need it.
 	*/
-	public void printMessageList(){}
+	public void printMessageList()
+	{
+		StringBuilder sb = new StringBuilder();
+		for(MessageLine line : LogList)
+		{
+			sb.append(line.message);
+			sb.append("\n");
+			sb.append(line.source);
+			sb.append("\n");
+		}
+	}
 	
 	/**
 	 * to add a message to the messagelist
 	*/
-	public void addMessage(String source, String message){}
+	public void addMessage(String source, String message)
+	{
+		LogList.addMessage(new MessageLine(message, source));
+	}
 
 }
