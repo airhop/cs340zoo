7ac464fe38d6739d80f71b0ded0bac2bb6bf8668
David Ostler <David Ostler>
2016-01-30 13:07:38 -0700

    serializer and deseralizer using gson.  Whoever compiles will need to add gson to their classpath

--- src/shared/serialization/Deserializer.java
+++ src/shared/serialization/Deserializer.java
@@ -1,12 +1,21 @@
 package shared.serialization;
 
+import client.model.GameModel;
+import com.google.gson.Gson;
+
 public class Deserializer {
 
 	/**
   	* deserializes the JSON formatted data from the server 
-  	* 
+  	* @return object, you must cast this object to the thing you are deseralizing.
   	*/
-	public void deserialize(){}
+	public Object deserialize(String json)
+	{
+		Gson gson = new Gson();
+		GameModel deserializeGame = gson.fromJson(json, GameModel.class);
+
+		return deserializeGame;
+	}
 
 
 }
--- src/shared/serialization/Serializer.java
+++ src/shared/serialization/Serializer.java
@@ -1,12 +1,21 @@
 package shared.serialization;
 
+import com.google.gson.Gson;
+
 public class Serializer{
 
 	/**
   	* serializes the JSON formatted to be sent to the server
   	* 
   	*/
-	public void serialize(){}
+	public String serialize(Object objectToSeralize)
+	{
+		Gson gson = new Gson();
+		String json = gson.toJson(objectToSeralize);
+
+		return json;
+
+	}
 
 
 }
