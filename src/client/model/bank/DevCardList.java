84567556c0b9217c52b6cd2694b91adf6e08a056
airhop <airhop12@gmail.com>
2016-02-02 20:41:44 -0700

    Update DevCardList.java

--- src/client/model/bank/DevCardList.java
+++ src/client/model/bank/DevCardList.java
@@ -55,34 +55,44 @@ public class DevCardList {
     return total;
   }
   
-  public int getMonopoly() {
-		return monopoly;
-	}
-	public void setMonopoly(int monopoly) {
-		this.monopoly = monopoly;
-	}
-	public int getMonument() {
-		return monument;
-	}
-	public void setMonument(int monument) {
-		this.monument = monument;
-	}
-	public int getRoadBuilding() {
-		return roadBuilding;
-	}
-	public void setRoadBuilding(int roadBuilding) {
-		this.roadBuilding = roadBuilding;
-	}
-	public int getSoldier() {
-		return soldier;
-	}
-	public void setSoldier(int soldier) {
-		this.soldier = soldier;
-	}
-	public int getYearOfPlenty() {
-		return yearOfPlenty;
-	}
-	public void setYearOfPlenty(int yearOfPlenty) {
-		this.yearOfPlenty = yearOfPlenty;
-	}
+  public int getMonopoly() 
+  {
+    return monopoly;
+  }
+  public void setMonopoly(int monopoly) 
+  {
+	this.monopoly = monopoly;
+  }
+  public int getMonument()
+  {
+	return monument;
+  }
+  public void setMonument(int monument)
+  {
+	this.monument = monument;
+  }
+  public int getRoadBuilding
+  {
+	return roadBuilding;
+  }
+  public void setRoadBuilding(int roadBuilding) 
+  {
+	this.roadBuilding = roadBuilding;
+  }
+  public int getSoldier()
+  {
+	return soldier;
+  }
+  public void setSoldier(int soldier) 
+  {
+	this.soldier = soldier;
+  }
+  public int getYearOfPlenty()
+  {
+	return yearOfPlenty;
+  }
+  public void setYearOfPlenty(int yearOfPlenty)
+  {
+	this.yearOfPlenty = yearOfPlenty;
+  }
 }
