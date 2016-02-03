8d399fdb4911a4efe4ad40b407e4819bae5258d2
airhop <airhop12@gmail.com>
2016-02-02 22:54:54 -0700

    Update PassObject.java

--- src/shared/jsonobject/PassObject.java
+++ src/shared/jsonobject/PassObject.java
@@ -7,6 +7,34 @@ public class PassObject
    String information;
    public PassObject()
    {
+      playerid = -1;
+      otherid = -1;
+      boolean acceptablility = false;
+      information = null;
    }
-
+   
+   public int getPlayerid() {
+		return playerid;
+	}
+	public void setPlayerid(int playerid) {
+		this.playerid = playerid;
+	}
+	public int getOtherid() {
+		return otherid;
+	}
+	public void setOtherid(int otherid) {
+		this.otherid = otherid;
+	}
+	public boolean isAcceptability() {
+		return acceptability;
+	}
+	public void setAcceptability(boolean acceptability) {
+		this.acceptability = acceptability;
+	}
+	public String getInformation() {
+		return information;
+	}
+	public void setInformation(String information) {
+		this.information = information;
+	}
 }
