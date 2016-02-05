b1e2b7801d2a04888a3cc7c40f7ca96149191e60
Saladin926 <davidtostler@gmail.com>
2016-02-05 16:53:32 -0700

    Update PollerTest.java
    hopefully works

--- src/shared/test/PollerTest.java
+++ src/shared/test/PollerTest.java
@@ -4,16 +4,23 @@ import client.model.GameModel;
 import client.proxy.IProxy;
 import client.proxy.MockProxy;
 import org.junit.Test;
+import java.swing.Timer;
+import java.awt.event.*;
 
-/**
- * Created by GaryPetersen on 2/5/2016.
- */
 
-public class PollerTest {
+public class PollerTest 
+{
     @Test
-    public void pollerTest() {
-        GameModel gameModel = new GameModel();
-        IProxy iProxy = new MockProxy(gameModel);
-        Poller poller = new Poller(iProxy);
+    public void pollerTest() 
+    {
+        Poller poller = new Poller(new MockProxy(new GameModel());
+        timer = new Timer(30000, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent evt)
+            {
+                assert(amount == 9 || amount == 10);
+            }
+        }
+        
     }
 }
