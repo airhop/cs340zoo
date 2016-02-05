a23f9a5da0af8e2dd3630a3b36ef2ed68c04d48b
Joshua <josh.w.nielsen@gmail.com>
2016-02-05 15:50:48 -0700

    Proxy

--- src/shared/test/CanDoTest.java
+++ src/shared/test/CanDoTest.java
@@ -1,15 +1,27 @@
 package shared.test;
 
+import client.model.GameModel;
+import client.proxy.IProxy;
+import client.proxy.Proxy;
 import org.junit.Test;
 
 /**
  * Created by GaryPetersen on 2/5/2016.
  */
-public class CanDoTest
-{
+public class CanDoTest {
+
+    private GameModel myGameModel = new GameModel();
+    private IProxy myProxy = new Proxy(myGameModel);
+    private int playerIndex;
+    private int playerId;
+
+    public CanDoTest(){
+
+    }
+
+
     @Test
-    public testCanBuildSettlement()
-    {
+    public void testCanBuildSettlement() {
 
     }
 }
--- src/shared/test/ProxyTest.java
+++ src/shared/test/ProxyTest.java
@@ -1,7 +1,74 @@
 package shared.test;
 
+import org.junit.Test;
+
 /**
  * Created by GaryPetersen on 2/5/2016.
  */
 public class ProxyTest {
+
+    @Test
+    public void testGameModelBefore(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+    @Test
+    public void testGameModelAfter(){
+
+    }
+
+
+
+
 }
