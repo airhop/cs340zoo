92fe7982f54bac90cae920410539ea862beac1c1
KasiKarra <hate2shop@sbcglobal.net>
2016-02-05 13:53:06 -0700

    Port update again . . .

--- src/shared/test/CanDoTest.java
+++ src/shared/test/CanDoTest.java
@@ -1,7 +1,15 @@
 package shared.test;
 
+import org.junit.Test;
+
 /**
  * Created by GaryPetersen on 2/5/2016.
  */
-public class CanDoTest {
+public class CanDoTest
+{
+    @Test
+    public testCanBuildSettlement()
+    {
+
+    }
 }
