44696cccc62ccbe66ebe0b2e4838e22afe05caa0
airhop <airhop12@gmail.com>
2016-02-05 17:31:10 -0700

    Update Hex.java

--- src/client/model/map/Hex.java
+++ src/client/model/map/Hex.java
@@ -3,36 +3,21 @@ package client.model.map;
 import shared.definitions.HexType;
 
 public class Hex {
-  
-  shared.locations.HexLocation location;
   String resource;
   int chit;
 
-  public Hex(int x, int y)//desert tile
+  public Hex()//desert tile
   {
-    location = new  shared.locations.HexLocation(x,y);
     resource = HexType.DESERT.toString();
     chit = 0;
   }
 
-  public Hex(int x, int y, String resrc, int num)
+  public Hex(String resrc, int num)
   {
-    location = new  shared.locations.HexLocation(x,y);
     resource = resrc.toUpperCase();
     chit = num;
   }
   //Getters and Setters
-
-
-  public shared.locations.HexLocation getLocation() 
-  {
-	  return location;
-  }
-
-  public void setLocation(shared.locations.HexLocation location) 
-  {
-	  this.location = location;
-  }
 
   public String getResource() 
   {
