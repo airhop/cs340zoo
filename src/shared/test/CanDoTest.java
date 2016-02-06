e849658848eecb2238ad33b4587f07d5e33c1778
David Ostler <David Ostler>
2016-02-05 18:25:17 -0700

    fixed some implementation that had disappeared

--- src/shared/test/CanDoTest.java
+++ src/shared/test/CanDoTest.java
@@ -167,24 +167,42 @@ public class CanDoTest
     }
 
 
-//    @Test
-//    public void testCanDiscardCards()
-//    {
-//        initializeFull();
-//        TurnTracker turnTracker = f.getGM().getTt();
-//        ResourceList resourceList = new ResourceList(1,);
-//        turnTracker.getCurrentPlayer(1);
-//        f.getGM().setTt(turnTracker);
-//
-//        assert(f.canDiscardCards(1))
-//
-//    }
+    @Test
+    public void testCanDiscardCards()
+    {
+        initializeFull();
+        TurnTracker turnTracker = f.getGM().getTt();
+        ResourceList resourceList = new ResourceList(1,5,2,6,4);
+        turnTracker.setCurrentPlayer(1);
+        f.getGM().setTt(turnTracker);
+        assert(f.canDiscardCards(1,resourceList));
+        assert(f.canDiscardCards(0,resourceList));
+
+        turnTracker.setCurrentPlayer(0);
+        f.getGM().setTt(turnTracker);
+        ResourceList emptyList = new ResourceList(0,0,0,0,0);
+        f.getGM().getPlayers().get(0).setResources(emptyList);
+        assert(!f.canDiscardCards(0,resourceList));
+
+    }
 
 
     @Test
     public void testCanRollNumber()
     {
+        initializeFull();
+        TurnTracker turnTracker = f.getGM().getTt();
+        turnTracker.setCurrentPlayer(1);
+        f.getGM().setTt(turnTracker);
 
+        assert (f.canRoll(1));
+        assert (!f.canRoll(0));initializeFull();
+        turnTracker = f.getGM().getTt();
+        turnTracker.setCurrentPlayer(1);
+        f.getGM().setTt(turnTracker);
+
+        assert (f.canRoll(1));
+        assert (!f.canRoll(0));
     }
 
 
