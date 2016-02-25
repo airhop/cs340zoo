80a76ef838dd0ea25ccef1e585106c64d8a7b130
airhop <airhop12@gmail.com>
2016-02-25 16:36:05 -0700

    Update Cookie.java

--- src/client/proxy/Cookie.java
+++ src/client/proxy/Cookie.java
@@ -55,36 +55,30 @@ public class Cookie {
 
     public String getPlayerId() {
         String temp;
-        if (decodedValue.equals("")) {
-            getDecode();
-        }
-        JsonParser myParse = new JsonParser();
-        JsonElement myEle = myParse.parse(decodedValue);
-        JsonTreeReader myTree = new JsonTreeReader(myEle);
-        try {
-            if (myTree.hasNext()) {
-                myTree.beginObject();
-                myTree.nextName();
-                myTree.nextString();
-                myTree.nextName();
-                myTree.nextString();
-                myTree.nextName();
-                System.out.println(myTree.peek().name());
-                if(!myTree.peek().name().equals("STRING")){
-                    myId = myTree.nextString();
-                }else{
+                if (myId.equals("")) {
+ -            if (decodedValue.equals("")) {
+ -                getDecode();
+ -            }
+ -            JsonParser myParse = new JsonParser();
+ -            JsonElement myEle = myParse.parse(decodedValue);
+ -            JsonTreeReader myTree = new JsonTreeReader(myEle);
+ -            try {
+ -                if (myTree.hasNext()) 
+                    {
+ -                  myTree.beginObject();
+ -                  myTree.nextName();
+ -                  myTree.nextString();
+ -                  myTree.nextName();
                     myTree.nextString();
                     myTree.nextName();
                     myId = myTree.nextString();
-                }
+                    System.out.println(myId);
+                    }
+                return myId;
+                  } catch (IOException e) { e.printStackTrace();}
 
-
-                System.out.println(myId);
-            }
+            } else{
             return myId;
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return myId;
