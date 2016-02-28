1f415c0d557e33a936b6047c1b857c6e0fbe3603
David Ostler <David Ostler>
2016-02-27 17:17:34 -0700

    player waiting is now working

--- src/client/MVC/join/PlayerWaitingController.java
+++ src/client/MVC/join/PlayerWaitingController.java
@@ -1,9 +1,13 @@
 package client.MVC.join;
 
 import client.MVC.base.*;
+import client.MVC.data.PlayerInfo;
 import client.facade.Facade;
 import client.model.GameModel;
+import client.model.player.Player;
+import shared.definitions.CatanColor;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 
@@ -26,18 +30,30 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
             Facade.getInstance().setReady();
         return numPlayers;
     }
+    private PlayerInfo[] playersInfo()
+    {
+        PlayerInfo[] players = new PlayerInfo[getNumPlayers()];
 
+        for(int i = 0; i < getNumPlayers(); i++)
+        {
+            players[i] = Facade.getInstance().gamesList().get(Facade.getInstance().getGameModel().getID()).getPlayers().get(i);
+        }
+        return players;
+    }
     @Override
     public IPlayerWaitingView getView() {
 
         return (IPlayerWaitingView) super.getView();
     }
+    
 
     @Override
     public void start() {
         System.out.println("the number of current players: ");
         System.out.println(getNumPlayers());
 
+
+        getView().setPlayers(playersInfo());
         if (getNumPlayers() < 4) 
         {
             getView().showModal();
@@ -65,7 +81,7 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
     @Override
     public void update(Observable o, Object arg) {
         GameModel game = (GameModel)o;
-        if(Facade.getInstance().gamesList().get(game.getID()).getPlayers().size() >= 4)
+        if(Facade.getInstance().gamesList().get(game.getID()).getPlayers().size() >= 4 && game.getTurnTracker().getStatus() == "Waiting")
         {
             getView().closeModal();
         }
