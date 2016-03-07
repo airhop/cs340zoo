fa57484375d0519fcd52a5317f258ed05839224f
airhop <airhop12@gmail.com>
2016-03-06 23:40:24 -0700

    domestic trade

--- src/client/MVC/domestic/DomesticTradeController.java
+++ src/client/MVC/domestic/DomesticTradeController.java
@@ -429,8 +429,27 @@ public class DomesticTradeController extends Controller implements IDomesticTrad
             if (to != null) {
                 if (to.getReciever() == Facade.getInstance().getCurrentPlayer().getPlayerIndex()) {
                     if (!getAcceptOverlay().isModalShowing()) {
+                        if (brick > 0) acceptOverlay.addGetResource(ResourceType.BRICK, brick);
+                        else if (brick < 0) acceptOverlay.addGiveResource(ResourceType.BRICK, brick);
+
+                        if (ore > 0) acceptOverlay.addGetResource(ResourceType.ORE, ore);
+                        else if (ore < 0) acceptOverlay.addGiveResource(ResourceType.ORE, ore);
+
+                        if (sheep > 0) acceptOverlay.addGetResource(ResourceType.SHEEP, sheep);
+                        else if (sheep < 0) acceptOverlay.addGiveResource(ResourceType.SHEEP, sheep);
+
+                        if (wheat > 0) acceptOverlay.addGetResource(ResourceType.WHEAT, wheat);
+                        else if (wheat < 0) acceptOverlay.addGiveResource(ResourceType.WHEAT, wheat);
+
+                        if (wood > 0) acceptOverlay.addGetResource(ResourceType.WOOD, wood);
+                        else if (wood < 0) acceptOverlay.addGiveResource(ResourceType.WOOD, wood);
                         getAcceptOverlay().showModal();
                         getAcceptOverlay().setAcceptEnabled(Facade.getInstance().canAcceptTrade());
+                        if (getWaitOverlay().isModalShowing()) {
+                            if (to.getReciever() == 0 && to.getSender() == 0) {
+                                getWaitOverlay().closeModal();
+                            }
+                        }
                     }
                 }
             }
