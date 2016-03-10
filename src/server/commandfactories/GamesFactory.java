e7fff5b69ea2b9582b8a81186aa1d74c04b9a21b
Joshua <josh.w.nielsen@gmail.com>
2016-03-10 16:54:58 -0700

    Working on UserFactory

--- src/server/commandfactories/GamesFactory.java
+++ src/server/commandfactories/GamesFactory.java
@@ -1,5 +1,9 @@
 package server.commandfactories;
 
+import server.commandobjects.ICommand;
+import server.commandobjects.moves.AcceptTrade;
+import server.servermain.JsonConstructionInfo;
+
 /**
  * Created by Joshua on 3/10/2016.
  */
--- src/server/commandfactories/UserFactory.java
+++ src/server/commandfactories/UserFactory.java
@@ -12,9 +12,9 @@ public class UserFactory {
 
     }
     /**
-     *
-     * @param info -
-     * @return -
+     * This is the method that you call when you want a Command object that is in the User category
+     * @param info - This is the information that is passed to the server so that the factory can create the specified object
+     * @return - The command Object that we are asking for.
      */
     public ICommand getCommand(JsonConstructionInfo info){
         ICommand command = new AcceptTrade(1, false);
