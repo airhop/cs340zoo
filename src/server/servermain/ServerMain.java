46ce60b4b8570473bbc53cb43b4e152f98dbf93d
Joshua <josh.w.nielsen@gmail.com>
2016-03-09 17:01:20 -0700

    Added Phase 3 ToDo's

---
+++ src/server/servermain/ServerMain.java
@@ -0,0 +1,7 @@
+package server.servermain;
+
+/**
+ * Created by Joshua on 3/9/2016.
+ */
+public class ServerMain {
+}
---
+++ src/server/servermain/handler/Handler.java
@@ -0,0 +1,7 @@
+package server.servermain.handler;
+
+/**
+ * Created by Joshua on 3/9/2016.
+ */
+public class Handler {
+}
