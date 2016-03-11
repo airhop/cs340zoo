3b4cc727759d035b54577e935dabaac09f098a71
David Ostler <David Ostler>
2016-03-10 21:44:19 -0700

    starting on GamesFactory

--- src/server/commandfactories/GamesFactory.java
+++ src/server/commandfactories/GamesFactory.java
@@ -2,10 +2,26 @@ package server.commandfactories;
 
 import server.commandobjects.ICommand;
 import server.commandobjects.moves.AcceptTrade;
+import server.commandobjects.games.*;
 import server.servermain.JsonConstructionInfo;
 
 /**
  * Created by Joshua on 3/10/2016.
  */
 public class GamesFactory {
+    public GamesFactory(){
+
+    }
+
+    /**
+     * This is the method that you call when you want a Command object that is in the games category
+     * @param info - This is the information that is passed to the server so that the factory can create the specified object
+     * @return - The command Object that we are asking for.
+     */
+    public ICommand getCommand(JsonConstructionInfo info){
+        ICommand commandObject;
+        commandObject = new Create(true,true,true,"GameName");
+
+        return commandObject;
+    }
 }
