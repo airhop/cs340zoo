9336ffd220b121ac46805a92fc28d20ba9981af4
KasiKarra <hate2shop@sbcglobal.net>
2016-03-11 12:22:35 -0700

    Update CommandType.java
    just missing maritimeTrade and discardCards

--- src/server/shared/CommandType.java
+++ src/server/shared/CommandType.java
@@ -4,5 +4,6 @@ package server.shared;
  * Created by Joshua on 3/10/2016.
  */
 public enum CommandType {
-    sendChat, rollNumber, robPlayer, finishTurn, buyDevCard, Year_Of_Plenty, Road_Building, Soldier, Monopoly, Monument, buildRoad, buildSettlement, buildCity, offerTrade, acceptTrade,
+    sendChat, rollNumber, robPlayer, finishTurn, buyDevCard, Year_Of_Plenty, Road_Building, Soldier, 
+        Monopoly, Monument, buildRoad, buildSettlement, buildCity, offerTrade, acceptTrade, maritimeTrade, discardCards
 }
