d8dd7de91905dca1af3d137812a3fa34592793c8
KasiKarra <hate2shop@sbcglobal.net>
2016-03-11 12:27:36 -0700

    commandType.convert added

--- src/server/servermain/handler/Handler.java
+++ src/server/servermain/handler/Handler.java
@@ -18,7 +18,7 @@ public class Handler implements HttpHandler
      * This method will grab the initial incoming exchange and parse the incoming Request Method
      * If it is a GET request it will pass it to the GetMethod.  Otherwise it will parse the RequestURI
      * and pass it to the User, Game, or Moves method
-     * @param exchange
+     * @param exchange - incoming request
      * @throws IOException
      */
     public void handle(HttpExchange exchange) throws IOException //probably change to be a ServerException
@@ -29,7 +29,7 @@ public class Handler implements HttpHandler
     /**
      * This method will determine whether this is a game/model or games/list method and will
      * set the exchange in preperation to send it back
-     * @param exchange
+     * @param exchange - incoming request passed by the handle method
      */
     public void Get(HttpExchange exchange)
     {
@@ -39,7 +39,7 @@ public class Handler implements HttpHandler
     /**
      * This method will determine whether this is a user/login or user/register method before
      * passing the JSON information to the commandfactory to be processed
-     * @param exchange
+     * @param exchange - incoming request passed by the handle method
      */
     public void UserMethod(HttpExchange exchange)
     {
@@ -47,9 +47,9 @@ public class Handler implements HttpHandler
     }
 
     /**
-     * This method will handle game and games methods that will be implemented.
+     * This method will handle games Post methods that will be implemented.
      * It will pass the JSON information to the commandfactory to be processed
-     * @param exchange
+     * @param exchange - incoming request passed by the handle method
      */
     public void GameMethod(HttpExchange exchange)
     {
@@ -59,7 +59,7 @@ public class Handler implements HttpHandler
     /**
      * This method will parse the RequestURI and send the necessary information to the
      * commandfactory to be made into commandObjects
-     * @param exchange
+     * @param exchange - incoming request passed by the handle method
      */
     public void MoveMethod(HttpExchange exchange)
     {
--- src/server/shared/CommandType.java
+++ src/server/shared/CommandType.java
@@ -5,5 +5,18 @@ package server.shared;
  */
 public enum CommandType {
     sendChat, rollNumber, robPlayer, finishTurn, buyDevCard, Year_Of_Plenty, Road_Building, Soldier, 
-        Monopoly, Monument, buildRoad, buildSettlement, buildCity, offerTrade, acceptTrade, maritimeTrade, discardCards
+        Monopoly, Monument, buildRoad, buildSettlement, buildCity, offerTrade, acceptTrade, maritimeTrade, discardCards;
+
+
+    /**
+     * The type will come in through the handler as a string from the URI  This method will return
+     * the correct commandType so that it can be passed to the commandFactory correctly
+     *
+     * @param type - incoming type that needs to be converted to an enum
+     * @return the associated CommandType
+     */
+    public CommandType convert(String type)
+    {
+        return sendChat;
+    }
 }
