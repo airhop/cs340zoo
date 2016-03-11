b4dd39469114b9b32c82673d6cf9a0b0eb9cf230
MikeLo <mike.hermes@me.com>
2016-03-11 02:58:30 -0700

    IServerFacade.java is done, MockServerFacade and ServerFacade r the same

--- src/server/serverfacade/IServerFacade.java
+++ src/server/serverfacade/IServerFacade.java
@@ -1,46 +1,51 @@
 package server.serverfacade;
 
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+import client.model.GameModel;
+import client.model.bank.ResourceList;
+import client.model.player.Player;
+
 /**
  * Created by Joshua on 3/9/2016.
  */
 public interface IServerFacade 
 {
 	// user: total 2 methods
-	void postLogin();
-	void postRegister();
+	void postLogin(String username, String password);
+	void postRegister(String username, String password);
 	//games: total 5 methods
-	void getList();
-	void postCreate();
-	void postJoin();
-	void postSave();
-	void postLoad();
+	GameModel getList();
+	void postCreate(String title, int id, Player[] players);
+	void postJoin(int id, String color);
+	void postSave(int id, String name);
+	void postLoad(String name);
 	//game: total 6 methods
-	void getModel(); //FYI getModel is the only required method from this section.
-					//the rest are crossed out on the Phase specs
-
-	void postRest(); //I think you meant reset not rest
+	GameModel getModel();
+	void postRest();
 	void postCommands();
 	void getCommands();
-	void postAddAI();
+	void postAddAI(String AIType);
 	void getListAI();
 	//moves: total 17 methods
-	void postSendChat();
-	void postRollNumber();
-	void postRobPlayer();
-	void postFinishTurn();
-	void postBuyDevCard();
-	void postYearOfPlenty();
-	void postRoadBuilding();
-	void postSoldier();
-	void postMonopoly();
-	void postMonument();
-	void postBuildRoad();
-	void postBuildSettlement();
-	void postBuildCity();
-	void postOfferTrade();
-	void postAcceptTrade();
-	void postMaritimeTrade();
-	void postDiscardCards();
+	void postSendChat(int playerIndex, String content);
+	void postRollNumber(int playerIndex, int number);
+	void postRobPlayer(int plyerIndex, int victimIndex, HexLocation location);
+	void postFinishTurn(int playerIndex);
+	void postBuyDevCard(int playerIndex);
+	void postYearOfPlenty(int playerIndex, ResourceList res1, ResourceList res2);
+	void postRoadBuilding(int playerIndex, EdgeLocation spot1, EdgeLocation spot2);
+	void postSoldier(int playerIndex, int victimIndex, HexLocation location);
+	void postMonopoly(int playerIndex, String resource);
+	void postMonument(int playerIndex);
+	void postBuildRoad(int playerIndex, EdgeLocation roadLocation, Boolean free);
+	void postBuildSettlement(int playerIndex, VertexLocation vertexLocation, boolean free);
+	void postBuildCity(int playerIndex, VertexLocation vertexLocation);
+	void postOfferTrade(int playerIndex, ResourceList offer, int receiver);
+	void postAcceptTrade(int playerIndex, boolean willAccept);
+	void postMaritimeTrade(int playerIndex, int ratio, String inputResource, String outputResource);
+	void postDiscardCards(int playerIndex, ResourceList discardedCards);
 	//util: total 1 mehtod
-	void postChangeLogLevel();
+	void postChangeLogLevel(String logLevel);
 }
--- src/server/serverfacade/MockServerFacade.java
+++ src/server/serverfacade/MockServerFacade.java
@@ -1,9 +1,209 @@
 package server.serverfacade;
 
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+import client.model.GameModel;
+import client.model.bank.ResourceList;
+import client.model.player.Player;
+
 /**
  * Created by Josh on 3/10/2016.
  */
-public class MockServerFacade 
+public class MockServerFacade implements IServerFacade
 {
+
+	@Override
+	public void postLogin(String username, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postRegister(String username, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GameModel getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void postCreate(String title, int id, Player[] players) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postJoin(int id, String color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSave(int id, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postLoad(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GameModel getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void postRest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postCommands() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCommands() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postAddAI(String AIType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getListAI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSendChat(int playerIndex, String content) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postRollNumber(int playerIndex, int number) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postRobPlayer(int plyerIndex, int victimIndex,
+			HexLocation location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postFinishTurn(int playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postBuyDevCard(int playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postYearOfPlenty(int playerIndex, ResourceList res1,
+			ResourceList res2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postRoadBuilding(int playerIndex, EdgeLocation spot1,
+			EdgeLocation spot2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSoldier(int playerIndex, int victimIndex,
+			HexLocation location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postMonopoly(int playerIndex, String resource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postMonument(int playerIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postBuildRoad(int playerIndex, EdgeLocation roadLocation,
+			Boolean free) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postBuildSettlement(int playerIndex,
+			VertexLocation vertexLocation, boolean free) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postBuildCity(int playerIndex, VertexLocation vertexLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postOfferTrade(int playerIndex, ResourceList offer, int receiver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postAcceptTrade(int playerIndex, boolean willAccept) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postMaritimeTrade(int playerIndex, int ratio,
+			String inputResource, String outputResource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postDiscardCards(int playerIndex, ResourceList discardedCards) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postChangeLogLevel(String logLevel) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
