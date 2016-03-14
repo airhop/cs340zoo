cbb5ba0680a4ab435c33fb0be590937aa1ff9644
Joshua <josh.w.nielsen@gmail.com>
2016-03-14 12:23:14 -0600

    Server Done ish ha

--- src/server/servermain/Server.java
+++ src/server/servermain/Server.java
@@ -3,7 +3,6 @@ package server.servermain;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import server.servermain.handler.Handler;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
@@ -29,10 +28,41 @@ public class Server {
             e.printStackTrace();
         }
 
-
         server.setExecutor(null); // use the default executor
 
-        server.createContext("/DownloadBatchHandler", mainHandler);
+
+
+
+        server.createContext("/user/login", mainHandler);
+        server.createContext("/user/register", mainHandler);
+
+        server.createContext("/games/list", mainHandler);
+        server.createContext("/games/create", mainHandler);
+        server.createContext("/games/join", mainHandler);
+        server.createContext("/games/save", mainHandler);
+        server.createContext("/games/load", mainHandler);
+
+        server.createContext("/game/model", mainHandler);
+
+        server.createContext("/moves/sendChat", mainHandler);
+        server.createContext("/moves/rollNumber", mainHandler);
+        server.createContext("/moves/robPlayer", mainHandler);
+        server.createContext("/moves/finishTurn", mainHandler);
+        server.createContext("/moves/buyDevCard", mainHandler);
+        server.createContext("/moves/Year_of_Plenty", mainHandler);
+        server.createContext("/moves/Road_Building", mainHandler);
+        server.createContext("/moves/Soldier", mainHandler);
+        server.createContext("/moves/Monopoly", mainHandler);
+        server.createContext("/moves/Monument", mainHandler);
+        server.createContext("/moves/buildRoad", mainHandler);
+        server.createContext("/moves/buildsettlement", mainHandler);
+        server.createContext("/moves/buildCity", mainHandler);
+        server.createContext("/moves/offerTrade", mainHandler);
+        server.createContext("/moves/acceptTrade", mainHandler);
+        server.createContext("/moves/maritimeTrade", mainHandler);
+        server.createContext("/moves/discardCards", mainHandler);
+
+
 //        logger.info("Starting HTTP Server");
         server.start();
     }
