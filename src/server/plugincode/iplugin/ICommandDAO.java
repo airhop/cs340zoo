8dba69744d4bea3c641290646de99f8a7d8b2715
KasiKarra <hate2shop@sbcglobal.net>
2016-04-02 14:25:09 -0600

    setup fixedish and cookies attempted

--- src/server/plugincode/iplugin/ICommandDAO.java
+++ src/server/plugincode/iplugin/ICommandDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
  * Created by Joshua on 4/2/2016.
  */
 public interface ICommandDAO {
-    void addCommand(ICommand command);
+    void addCommand(ICommand command, int gameId);
     List<ICommand> readAllCommands(int gameId);
     void clearGame(int gameId);
     void clearAll();
--- src/server/plugincode/mongodb/CommandDAO.java
+++ src/server/plugincode/mongodb/CommandDAO.java
@@ -2,9 +2,12 @@ package server.plugincode.mongodb;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import server.commandobjects.ICommand;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import server.plugincode.iplugin.ICommandDAO;
+import server.commandobjects.ICommand;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,24 +25,43 @@ public class CommandDAO implements ICommandDAO {
     @Override
     public void addCommand(ICommand command, int gameID)
     {
-
+        BasicDBObject bdbo = new BasicDBObject();
+        bdbo.put("Command", command);
+        bdbo.put("ID", gameID);
+        mdb.getCollection("Commands").insert(bdbo);
     }
 
     @Override
-    public List<ICommand> readAllCommands()
+    public List<ICommand> readAllCommands(int gameId)
     {
-        return null;
+        BasicDBObject bdbo = new BasicDBObject();
+        bdbo.put("ID", gameId);
+
+        DBCursor dbc = mdb.getCollection("Commands").find(bdbo);
+
+        List<ICommand> commands = new ArrayList<ICommand>();
+
+        while(dbc.hasNext())
+        {
+            DBObject dbo = dbc.next();
+            ICommand c = (ICommand)dbo.get("Command");
+            commands.add(c);
+        }
+
+        return new ArrayList<ICommand>();
     }
 
     @Override
     public void clearGame(int gameId)
     {
-
+        BasicDBObject bdbo = new BasicDBObject();
+        bdbo.put("ID", gameId);
+        mdb.getCollection("Commands").remove(bdbo);
     }
 
     @Override
     public void clearAll()
     {
-
+        mdb.getCollection("Commands").drop();
     }
 }
