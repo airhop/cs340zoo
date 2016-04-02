c8186e05bf0b9bd9c0e88de9574d8aee6c219db9
Joshua <josh.w.nielsen@gmail.com>
2016-04-02 13:06:27 -0600

    ColorsFixed

--- src/server/plugincode/iplugin/IPlayerDAO.java
+++ src/server/plugincode/iplugin/IPlayerDAO.java
@@ -1,5 +1,6 @@
 package server.plugincode.iplugin;
 
+import client.MVC.data.PlayerInfo;
 import client.model.player.Player;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
  * Created by Joshua on 4/2/2016.
  */
 public interface IPlayerDAO {
-    void addPlayer();
+    void addPlayer(PlayerInfo playerInfo);
     List<Player> readAllPlayers();
     void clearTable();
 }
