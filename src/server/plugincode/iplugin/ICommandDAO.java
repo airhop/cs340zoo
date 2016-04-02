f0cbafa2bd7c9a5e40761fc4e3df9abb77ab2e63
Joshua <josh.w.nielsen@gmail.com>
2016-04-02 12:43:46 -0600

    ColorsFixed

--- src/server/plugincode/iplugin/ICommandDAO.java
+++ src/server/plugincode/iplugin/ICommandDAO.java
@@ -1,7 +1,15 @@
 package server.plugincode.iplugin;
 
+import server.commandobjects.ICommand;
+
+import java.util.List;
+
 /**
  * Created by Joshua on 4/2/2016.
  */
 public interface ICommandDAO {
+    void addCommand(ICommand command);
+    List<ICommand> readAllCommands();
+    void clearGame(int gameId);
+    void clearAll();
 }
--- src/server/plugincode/iplugin/IGameDAO.java
+++ src/server/plugincode/iplugin/IGameDAO.java
@@ -1,7 +1,13 @@
 package server.plugincode.iplugin;
 
+import client.model.GameModel;
+
 /**
  * Created by Joshua on 4/2/2016.
  */
 public interface IGameDAO {
+    void addGame(GameModel addModel);
+    GameModel readGame(int gameId);
+    void updateGame(int gameId, GameModel updateModel);
+    void clearTable();
 }
