b151baec7e0e92cfa36f2bbd2b9b2928cdc97935
Joshua <josh.w.nielsen@gmail.com>
2016-04-02 14:03:45 -0600

    ColorsFixed

--- src/server/plugincode/registry/PluginDescriptor.java
+++ src/server/plugincode/registry/PluginDescriptor.java
@@ -9,7 +9,6 @@ import java.util.List;
  * Created by The Best Couple Ever on 4/2/2016.
  */
 public class PluginDescriptor {
-
     String name;
     String classPath;
     String description;
--- src/server/plugincode/registry/PluginRegistry.java
+++ src/server/plugincode/registry/PluginRegistry.java
@@ -8,10 +8,23 @@ import java.util.List;
  * Created by The Best Couple Ever on 4/2/2016.
  */
 public class PluginRegistry {
-    public void registerPlugin(PluginDescriptor inDescriptor){}
-    public List<PluginDescriptor> getAvailablePlugins(){return null;}
-    public IPersistencePlugin createPlugin(PluginDescriptor inDescriptor){return null;}
-    public void unregisterPlugin(PluginDescriptor inDescriptor){}
-    public void loadConfig(){}
-    public void saveConfig(){}
+    public void registerPlugin(PluginDescriptor inDescriptor) {
+    }
+
+    public List<PluginDescriptor> getAvailablePlugins() {
+        return null;
+    }
+
+    public IPersistencePlugin createPlugin(PluginDescriptor inDescriptor) {
+        return null;
+    }
+
+    public void unregisterPlugin(PluginDescriptor inDescriptor) {
+    }
+
+    public void loadConfig() {
+    }
+
+    public void saveConfig() {
+    }
 }
