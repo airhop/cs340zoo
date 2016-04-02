00e751f1d8aa41a3ed716d51e0fe0c27122e8283
David Ostler <David Ostler>
2016-04-02 13:59:45 -0600

    plugin stuff

---
+++ src/server/plugincode/registry/PluginDescriptor.java
@@ -0,0 +1,16 @@
+package server.plugincode.registry;
+
+import server.plugincode.iplugin.IPersistencePlugin;
+import sun.plugin2.main.server.Plugin;
+
+import java.util.List;
+
+/**
+ * Created by The Best Couple Ever on 4/2/2016.
+ */
+public class PluginDescriptor {
+
+    String name;
+    String classPath;
+    String description;
+}
---
+++ src/server/plugincode/registry/PluginRegistry.java
@@ -0,0 +1,17 @@
+package server.plugincode.registry;
+
+import server.plugincode.iplugin.IPersistencePlugin;
+
+import java.util.List;
+
+/**
+ * Created by The Best Couple Ever on 4/2/2016.
+ */
+public class PluginRegistry {
+    public void registerPlugin(PluginDescriptor inDescriptor){}
+    public List<PluginDescriptor> getAvailablePlugins(){return null;}
+    public IPersistencePlugin createPlugin(PluginDescriptor inDescriptor){return null;}
+    public void unregisterPlugin(PluginDescriptor inDescriptor){}
+    public void loadConfig(){}
+    public void saveConfig(){}
+}
