40b11b5a54b29f24380116bbeb8dd179d3a35314
Joshua <josh.w.nielsen@gmail.com>
2016-04-04 10:26:56 -0600

    ColorsFixed

--- src/server/plugincode/sql/CommandDAO.java
+++ src/server/plugincode/sql/CommandDAO.java
@@ -9,21 +9,39 @@ import java.util.List;
  * Created by Joshua on 4/2/2016.
  */
 public class CommandDAO implements ICommandDAO {
+
+    /**
+     * Adds a command to the command table
+     * @param command Command to add
+     * @param gameId Game command to be added
+     */
     @Override
-    public void addCommand(ICommand command) {
+    public void addCommand(ICommand command, int gameId) {
 
     }
 
+    /**
+     * Takes all commands from the database
+     * @param gameId game to get the commands for
+     * @return list of all commands
+     */
     @Override
-    public List<ICommand> readAllCommands() {
+    public List<ICommand> readAllCommands(int gameId) {
         return null;
     }
 
+    /**
+     * removes a game, may not need this
+     * @param gameId GameId to be dropped
+     */
     @Override
     public void clearGame(int gameId) {
 
     }
 
+    /**
+     * Drops all tables
+     */
     @Override
     public void clearAll() {
 
