abc36468bbe9b66de7741e727e7e7ba0551f9de1
David Ostler <David Ostler>
2016-04-10 20:21:19 -0600

    registry

--- src/server/plugincode/registry/PluginDescriptor.java
+++ src/server/plugincode/registry/PluginDescriptor.java
@@ -1,6 +1,7 @@
 package server.plugincode.registry;
 
 import server.plugincode.iplugin.IPersistencePlugin;
+import sun.plugin2.main.server.Plugin;
 //import sun.plugin2.main.server.Plugin;
 
 import java.util.List;
@@ -9,6 +10,14 @@ import java.util.List;
  * Created by The Best Couple Ever on 4/2/2016.
  */
 public class PluginDescriptor {
+
+    public PluginDescriptor(String name, String classpath, String description)
+    {
+        this.name = name;
+        this.classPath = classpath;
+        this.description = description;
+    }
+
     String name;
     String classPath;
     String description;
--- src/server/plugincode/registry/PluginRegistry.java
+++ src/server/plugincode/registry/PluginRegistry.java
@@ -2,17 +2,33 @@ package server.plugincode.registry;
 
 import server.plugincode.iplugin.IPersistencePlugin;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 /**
  * Created by The Best Couple Ever on 4/2/2016.
  */
 public class PluginRegistry {
+    public void setPluginDescriptors(List<PluginDescriptor> pluginDescriptors) {
+        this.pluginDescriptors = pluginDescriptors;
+    }
+
+    public List<PluginDescriptor> getPluginDescriptors() {
+        return pluginDescriptors;
+    }
+
     /**
      * Method will be called to register a plugin to be created
      * @param inDescriptor - the descriptor to be registered
      */
-    public void registerPlugin(PluginDescriptor inDescriptor) {
+    List<PluginDescriptor> pluginDescriptors = null;
+    public void registerPlugin(PluginDescriptor inDescriptor)
+    {
+
     }
 
     /**
@@ -20,7 +36,7 @@ public class PluginRegistry {
      * @return the list of available plugins
      */
     public List<PluginDescriptor> getAvailablePlugins() {
-        return null;
+        return this.pluginDescriptors;
     }
 
     /**
@@ -29,7 +45,22 @@ public class PluginRegistry {
      * @return the plugin that matches the descriptor
      */
     public IPersistencePlugin createPlugin(PluginDescriptor inDescriptor) {
-        return null;
+        IPersistencePlugin persPlugin = null;
+        ClassLoader classLoader = PluginRegistry.class.getClassLoader();
+
+        try {
+           Class aClass = (classLoader.loadClass("com.jenkov.MyClass"));
+           persPlugin = (IPersistencePlugin) aClass.getConstructors()[0].newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return persPlugin;
     }
 
     /**
@@ -42,8 +73,46 @@ public class PluginRegistry {
     /**
      * loads in the plugin config file that we will be putting the descriptors of the plugins
      */
-    public void loadConfig() {
+    public void loadConfig()
+    {
+        String fileName = "config.txt";
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                String stringLine = line.toString();
+                String[] tokens = stringLine.split(" ");
+                String desName = tokens[0];
+                String classpath = tokens[1];
+                String descriptor = tokens[2];
+                PluginDescriptor newDescriptor =  new PluginDescriptor(desName,classpath,descriptor);
+                this.pluginDescriptors.add(newDescriptor);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+        }
     }
+
 
     /**
      * not really used
